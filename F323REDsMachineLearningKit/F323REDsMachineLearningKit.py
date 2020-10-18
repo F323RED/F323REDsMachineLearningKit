148e7937923fc57a4d1e72d975d5b944618c0b8d
F323RED <f323red@gmail.com>
2020-10-18 16:13:38 +0800

    加入專案檔案。

---
+++ F323REDsMachineLearningKit/F323REDsMachineLearningKit.py
@@ -0,0 +1,70 @@
+'''
+Author : F323RED
+Date : 2020/10/17
+Version : 0.1.0
+Describes : A set of functions for machine learning projects.
+            Hopelly everything should be work.
+'''
+
+import numpy as np
+
+# Some common activation functions
+def Sigmoid(x) :
+    return (1 / (1 + np.exp(-x)))   # From 0 to 1, smoothly.
+
+def Stair(x) :
+    return x > 0                    # Either 1 or 0. Like a stair.
+
+def ReLU(x) :
+    return np.maximum(x, 0)         # Any postive real number.
+
+# Output process
+def Softmax(x) :
+    c = np.max(x)               # To prevent e^x overflow
+    exp_a = np.exp(x - c)
+    sum_exp = np.sum(exp_a)
+                                # The sum of every elements is 1.
+    return exp_a / sum_exp      # Represent chance of this answer.
+
+# Common loss functions
+# y : predict, t : correct answer
+def MeanSquareError(y, t) :
+    # Compatiable with batch process
+    if y.ndim == 1 :
+        t = t.reshape(1, t.size)        
+        y = y.reshape(1, y.size)
+
+    # Less is better.
+    return 0.5 * np.sum((y - t) ** 2) / y.shape[0]
+
+def CrossEntropyError(y, t) :
+    # Compatiable with batch process
+    if y.ndim == 1 :
+        t = t.reshape(1, t.size)        
+        y = y.reshape(1, y.size)
+
+    # Less is better.
+    return -np.sum(t * np.log(y + 10e-7))  / y.shape[0]
+
+# Some sort of differentiation stuff
+def NumericalDiff(f, x) :
+    h = 10e-4
+
+    return (f(x+h) - f(x-h)) / 2*h      # A simple differentiation
+
+def NumeriaclGradient(f, x) :
+    grad = np.zeros_like(x)
+
+    for i in range(x.size) :
+        grad[i] = NumericalDiff(f, x[i])
+
+    return grad;                        # Get the gradient at x
+
+def GradientDescent(f, init_x, learnRate=0.01, step=100) :
+    x = init_x
+
+    for i in range(step) :
+        grad = NumeriaclGradient(f, x)
+        x -= learnRate * grad
+
+    return x
